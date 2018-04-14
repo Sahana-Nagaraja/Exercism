69124fc43b512e0b72b3daab82fe6f46c025a78f
Sahana <nsahana@IN-nsahana.local>
2018-04-14 13:09:33 +0530

    Handshake calulator updated

---
+++ src/main/java/HandshakeCalculator.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class HandshakeCalculator {
+
+    List<Signal> calculateHandshake(int number) {
+        List<Signal> handshake = new ArrayList<Signal>();
+        if((number & 1) == 1) handshake.add(Signal.WINK);
+        if((number & 2) == 2) handshake.add(Signal.DOUBLE_BLINK);
+        if((number & 4) == 4) handshake.add(Signal.CLOSE_YOUR_EYES);
+        if((number & 8) == 8) handshake.add(Signal.JUMP);
+        if((number & 16) == 16) Collections.reverse(handshake);
+        return handshake;
+    }
+}
